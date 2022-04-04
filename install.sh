cd93c60cdadac8bb855a29a3ddaffe697b6b57aa
0xSP3C14LN1NJ4 <0xSP3C14LN1NJ4@gmail.com>
2022-04-04 10:26:37 -0400

    modify install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 echo "Creating necessary files"
 touch transactions.json
 touch transfers.json
-echo -e "Done!\n"
+touch balances.json
+echo "Done!\n"
 
 
 echo "Installing required libraries"
 pip install -r requirements.txt
-echo -e "Done!\n"
+echo "Done!\n"
 
 
 echo "Setting up Gemini API keys (you must already have created an API key for your main and/or a sandbox account)"
@@ -26,11 +27,11 @@ echo $api_key_test > api-key-test.txt
 echo "Enter your Gemini Sandbox API secret (leave blank if you only want to use your main account):"
 read api_secret_test
 echo $api_secret_test > api-secret-test.txt
-echo -e "Done!\n"
+echo "Done!\n"
 
 
 echo "Setting up the Cryptowatch API key (you must already have created an API key on cryptowat.ch)"
 echo "Enter your Cryptowatch API key:"
 read api_key_cw
 echo $api_key_cw > api-key-cw.txt
-echo -e "Done!\n"
+echo "Done!\n"
